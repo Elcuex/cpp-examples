72153327f76cfc499b505cc85ab60459c9c7716c
Elcuex <117916173+Elcuex@users.noreply.github.com>
2023-05-28 20:40:31 +0800

    Add closest year feature to leap_year.cpp

--- leap_year/leap_year.cpp
+++ leap_year/leap_year.cpp
@@ -3,7 +3,11 @@
 int main() {
     
     int year = 0;
+    int yearP = 0;
+    int yearM = 0;
+    int yearC = 0;
     bool repeat = true;
+    bool leap = false;
 
     while (repeat) {
     std::cout << "Please input desired four-digit number year: ";
@@ -16,20 +20,43 @@ int main() {
             std::cin >> year;
         }
 
+        if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
+            
+            std::cout << year << " is not a leap year :(\n";
+            
+            yearP = year;
+            yearM = year;
+            yearC = year;
+           
+            while((yearP % 4 != 0 || (yearP % 100 == 0 && yearP % 400 != 0)))
+            {
+                yearP++;
+                //std::cout << yearP << yearM;
+            }
+            
+            while(yearM % 4 != 0 || (yearM % 100 == 0 && yearM % 400 != 0))
+            {
+                yearM--;
+            }
 
-        if (year % 4 != 0) {
-            std::cout << year << " is not leap year :(\n";
-        }
+            std::cout << yearP << " is the next leap year!\n";
 
-        else if (year % 100 == 0 && year % 400 != 0) {
-            std::cout << year << " is not leap year :(\n";
+            std::cout << yearM << " is the previous leap year!\n";
+
+            if(abs(yearP - year) < abs((yearM - year))) {yearC = yearP;}
+            else {yearC = yearM;}
+
+            std::cout << yearC << " is the closest leap year!\n"; 
+
         }
 
         else {
             std::cout << year << " is a leap year!\n";
+            leap = true;
         }
 
         std::cout << "Repeat? (Enter 1 if yes): ";
         std::cin >> repeat;
+        leap = false;
     }
 }
